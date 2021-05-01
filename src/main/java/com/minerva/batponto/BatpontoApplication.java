240c0d88e0f10438c029ad0bf67ec6e27cec019c
Diana Amador Barreto <diana.barreto@meta.com.br>
2021-05-01 12:27:30 -0300

    first commit

---
+++ src/main/java/com/minerva/batponto/BatpontoApplication.java
@@ -0,0 +1,13 @@
+package com.minerva.batponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatpontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatpontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minerva/batponto/BatpontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minerva.batponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatpontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
